f66f91281ed19bf83f6a73e7f14e6871b807d9e3
sour-lollipop <dossymkhanova.a@gmail.com>
2023-06-11 20:38:40 +0600

    login

--- App.js
+++ App.js
@@ -1,82 +1,94 @@
 import React from "react";
-import { StyleSheet, TouchableOpacity, Button } from "react-native";
+import { StyleSheet, TouchableOpacity, Button, View, Text, TextInput } from "react-native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer, useNavigation } from "@react-navigation/native";
 import { MaterialCommunityIcons } from "@expo/vector-icons";
 import MainScreen from "./view/MainPage";
 import ProfileScreen from "./view/ProfilePage";
 import ScanScreen from "./view/ScanPage";
+import LoginPage from "./view/Login";
+
 
 const Tab = createBottomTabNavigator();
 
+let account = 0;
+
 const App = ({ navigation }) => {
-  return (
-    <>
-      <NavigationContainer>
-        <Tab.Navigator
-          style={styles.tabBar}
-          screenOptions={({ route }) => ({
-            tabBarIcon: ({ focused, color, size }) => {
-              let iconName;
+  if (account == 0) {
+    return (
+      <View style={styles.screen}>
+        <Text>This is the Login screen</Text>
+      </View>
+    );
 
-              if (route.name === "main") {
-                iconName = focused ? "home" : "home";
-              } else if (route.name === "scan") {
-                iconName = focused ? "qrcode-scan" : "qrcode-scan";
-              } else if (route.name === "profile") {
-                iconName = focused ? "account" : "account";
-              }
+  } else {
+    return (
+      <>
+        <NavigationContainer>
+          <Tab.Navigator
+            style={styles.tabBar}
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName;
 
-              return (
-                <MaterialCommunityIcons
-                  name={iconName}
-                  size={size}
-                  color={color}
-                />
-              );
-            },
-            tabBarLabel: "",
-            tabBarActiveTintColor: "#1573FE",
-            tabBarInactiveTintColor: "gray",
-          })}
-        >
-          <Tab.Screen
-            name="main"
-            component={MainScreen}
-            options={{ headerShown: false }}
-          />
-          <Tab.Screen
-            name="scan"
-            component={ScanScreen}
-            options={{
-              headerShown: false,
-              tabBarButton: (props) => (
-                <TouchableOpacity
-                  style={styles.scanButton}
-                  onPress={(e) => {
-                    props.onPress?.(e);
-                  }}
-                >
+                if (route.name === "main") {
+                  iconName = focused ? "home" : "home";
+                } else if (route.name === "scan") {
+                  iconName = focused ? "qrcode-scan" : "qrcode-scan";
+                } else if (route.name === "profile") {
+                  iconName = focused ? "account" : "account";
+                }
+
+                return (
                   <MaterialCommunityIcons
-                    name="qrcode-scan"
-                    color="white"
-                    size={30}
+                    name={iconName}
+                    size={size}
+                    color={color}
                   />
-                </TouchableOpacity>
-              ),
-            }}
-          />
-          <Tab.Screen
-            name="profile"
-            component={ProfileScreen}
-            options={{ headerShown: false }}
-          />
-        </Tab.Navigator>
-      </NavigationContainer>
-    </>
-  );
+                );
+              },
+              tabBarLabel: "",
+              tabBarActiveTintColor: "#1573FE",
+              tabBarInactiveTintColor: "gray",
+            })}
+          >
+            <Tab.Screen
+              name="main"
+              component={MainScreen}
+              options={{ headerShown: false }}
+            />
+            <Tab.Screen
+              name="scan"
+              component={ScanScreen}
+              options={{
+                headerShown: false,
+                tabBarButton: (props) => (
+                  <TouchableOpacity
+                    style={styles.scanButton}
+                    onPress={(e) => {
+                      props.onPress?.(e);
+                    }}
+                  >
+                    <MaterialCommunityIcons
+                      name="qrcode-scan"
+                      color="white"
+                      size={30}
+                    />
+                  </TouchableOpacity>
+                ),
+              }}
+            />
+            <Tab.Screen
+              name="profile"
+              component={ProfileScreen}
+              options={{ headerShown: false }}
+            />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </>
+    );
+  }
 };
-
 const styles = StyleSheet.create({
   scanButton: {
     width: 80,
---
+++ view/Login.js
@@ -0,0 +1,9 @@
+import { StyleSheet, Text, View, TextInput } from "react-native";
+
+const Login = ({ navigation }) => {
+  return (
+    <View>
+      <Text>This is the Login screen</Text>
+    </View>
+  );
+};
